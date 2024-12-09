d014283ed0cb026025d8abbfdf80326db303bcb1
Alberto <albertojunior848423803@gmail.com>
2024-12-09 21:28:31 +0300

    changes

--- src/main/java/com/springproject/registration/model/Student.java
+++ src/main/java/com/springproject/registration/model/Student.java
@@ -1,4 +1,23 @@
 package com.springproject.registration.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+
+import lombok.Data;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Data
+@Entity
 public class Student {
+
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @Column(name = "id", unique = true)
+    private String id;
+
+    private String name;
+    private String email;
+    private String adress;
 }
