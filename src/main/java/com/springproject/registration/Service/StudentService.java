0a9d88442f402e4a86b8f9aee73716b1b3b22ec6
Alberto <albertojunior848423803@gmail.com>
2024-12-13 00:42:48 +0300

    changes

--- src/main/java/com/springproject/registration/Service/StudentService.java
+++ src/main/java/com/springproject/registration/Service/StudentService.java
@@ -1,10 +1,10 @@
 package com.springproject.registration.Service;
 
-
 import com.springproject.registration.model.Student;
 import com.springproject.registration.repository.StudentRepository;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,35 +12,23 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class StudentService  {
+public class StudentService {
 
     private StudentRepository studentRepository;
 
-
-
-    public Student registerStudent(Student student){
-
+    public Student registerStudent(Student student) {
         return studentRepository.save(student);
     }
 
     public List<Student> getAllStudents(){
-
         return studentRepository.findAll();
     }
 
     public Optional<Student> getStudentById(String id){
-
         return studentRepository.findById(id);
     }
 
-    public Student getStudentByEmail(String email){
 
-        return studentRepository.findByEmail(email);
 
-    }
-
-    public void deleteStudent(String id){
-        studentRepository.deleteById(id);
-    }
 
 }
